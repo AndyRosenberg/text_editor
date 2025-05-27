e01531db77426e5b2d35c55ad27425b6e7e51aec
AndyRosenberg <andynaterosenberg@gmail.com>
2025-05-26 17:45:57 -0700

    clear screen and process key press

--- koji.c
+++ koji.c
@@ -5,6 +5,8 @@
 #include <termios.h>
 #include <unistd.h>
 
+#define CTRL_KEY(k) ((k) & 0x1f)
+
 struct termios orig_termios;
 
 void die(const char *s) {
@@ -32,36 +34,45 @@ void enable_raw_mode(void) {
   raw.c_cflag &= ~(CS8);
   raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
   raw.c_cc[VMIN] = 0;
-  raw.c_cc[VTIME] = 100;
+  raw.c_cc[VTIME] = 1;
 
   if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) {
     die("tcsetattr");
   }
 }
 
+char editor_read_key(void) {
+  int nread;
+  char c;
+
+  while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+    if (nread == -1 && errno != EAGAIN) {
+      die("read");
+    }
+  }
+
+  return c;
+}
+
+void editor_process_key_press(void) {
+  char c = editor_read_key();
+
+  switch (c) {
+    case CTRL_KEY('q'):
+      exit(0);
+      break;
+  }
+}
+
+void editor_refresh_screen(void) {
+  write(STDIN_FILENO, "\x1b[2J", 4);
+}
+
 int main(void) {
   enable_raw_mode();
 
   while (1) {
-    char c = '\0';
-
-    if (
-      read(STDIN_FILENO, &c, 1) == -1 &&
-        errno != EAGAIN
-    ) {
-      die("read");
-    }
-
-    if (iscntrl(c)) {
-      printf("%d\r\n", c);
-    } else {
-      printf("%d ('%c')\r\n", c, c);
-    }
-
-    if (c == 'q') {
-      break;
-    }
+    editor_refresh_screen();
+    editor_process_key_press();
   }
-
-  return 0;
 }
