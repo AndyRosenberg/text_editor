9cfefd0cdca6ac0efd1d46218adb13abd2cb4613
AndyRosenberg <andynaterosenberg@gmail.com>
2025-06-06 22:07:26 -0700

    multi-match navigation

--- koji.c
+++ koji.c
@@ -62,7 +62,7 @@ typedef struct {
 editor_cofig edconfig;
 
 /*** Prototypes ***/
-char *editor_prompt(char *prompt);
+char *editor_prompt(char *prompt, void(*callback)(char *, int));
 /******/
 
 void ab_append(append_buffer *ab, const char *s, int len) {
@@ -495,7 +495,7 @@ void editor_set_status_message(const char *fmt, ...) {
 
 void editor_save(void) {
   if (edconfig.file_name == NULL) {
-    edconfig.file_name = editor_prompt("Save as: %s (esc to cancel)");
+    edconfig.file_name = editor_prompt("Save as: %s (esc to cancel)", NULL);
     if (edconfig.file_name == NULL) {
       editor_set_status_message("Save aborted.");
       return;
@@ -535,20 +535,45 @@ void editor_save(void) {
   );
 }
 
-void editor_find(void) {
-  char *query = editor_prompt("Search: %s (esc to cancel)");
+void editor_find_callback(char *query, int key) {
+  static int last_match = -1;
+  static int direction = 1;
 
-  if (query == NULL) {
+  if (key == '\r' || key == '\x1b') {
+    last_match = -1;
+    direction = 1;
     return;
+  } else if (key == ARROW_RIGHT || key == ARROW_DOWN) {
+    direction = 1;
+  } else if (key == ARROW_LEFT || key == ARROW_UP) {
+    direction = -1;
+  } else {
+    last_match = -1;
+    direction = 1;
   }
 
+  if (last_match == -1) {
+    direction = 1;
+  }
+
+  int current_match = last_match;
   int i;
+
   for (i = 0; i < edconfig.number_of_rows; i++) {
-    editor_row *current_row = &edconfig.current_rows[i];
+    current_match += direction;
+
+    if (current_match == -1) {
+      current_match = edconfig.number_of_rows - 1;
+    } else if (current_match == edconfig.number_of_rows) {
+      current_match = 0;
+    }
+
+    editor_row *current_row = &edconfig.current_rows[current_match];
     char *match = strstr(current_row->render, (query));
 
     if (match) {
-      edconfig.cursor_y = i;
+      last_match = current_match;
+      edconfig.cursor_y = current_match;
       edconfig.cursor_x = editor_row_render_x_to_cursor_x(
         current_row,
         match - current_row->render
@@ -557,8 +582,27 @@ void editor_find(void) {
       break;
     }
   }
+}
 
-  free(query);
+void editor_find(void) {
+  int saved_cursor_x = edconfig.cursor_x;
+  int saved_cursor_y = edconfig.cursor_y;
+  int saved_column_offset = edconfig.column_offset;
+  int saved_row_offset = edconfig.row_offset;
+
+  char *query = editor_prompt(
+    "Search: %s (esc to cancel, arrows to navigate, enter to select)",
+    editor_find_callback
+  );
+
+  if (query) {
+    free(query);
+  } else {
+    edconfig.cursor_x = saved_cursor_x;
+    edconfig.cursor_y = saved_cursor_y;
+    edconfig.column_offset = saved_column_offset;
+    edconfig.row_offset = saved_row_offset;
+  }
 }
 
 void editor_scroll(void) {
@@ -769,7 +813,7 @@ int editor_read_key(void) {
   return c;
 }
 
-char *editor_prompt(char *prompt) {
+char *editor_prompt(char *prompt, void(*callback)(char *, int)) {
   size_t buffer_size = 128;
   char *buffer = malloc(buffer_size);
   size_t buffer_length = 0;
@@ -788,11 +832,20 @@ char *editor_prompt(char *prompt) {
       }
     } else if (c == '\x1b') {
       editor_set_status_message("");
+
+      if (callback) {
+        callback(buffer, c);
+      }
       free(buffer);
       return NULL;
     } else if (c == '\r') {
       if (buffer_length != 0) {
         editor_set_status_message("");
+
+        if (callback) {
+          callback(buffer, c);
+        }
+
         return buffer;
       }
     } else if (!iscntrl(c) && c < 128) {
@@ -802,6 +855,10 @@ char *editor_prompt(char *prompt) {
       }
       buffer[buffer_length++] = c;
       buffer[buffer_length] = '\0';
+    }
+
+    if (callback) {
+      callback(buffer, c);
     }
   }
 }
